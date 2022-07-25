6f8ee257947e20db6a6b5416eeaf747dd3d0bd16
Leonardo Kegel <leonardo.kegel@gmail.com>
2022-07-25 18:12:35 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const morgan = require('morgan');
+const apiRouter = require('./routes/apiRouter');
+const port = process.env.PORT || 3333;
+const app = express();
+
+app.use(morgan('tiny'));
+app.use('/usuarios/v1/', apiRouter)
+app.listen(port, () => {
+    console.info('App rodando na porta 3333');
+});
